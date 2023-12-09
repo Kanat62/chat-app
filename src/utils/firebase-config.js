889431c5068c729f662bf2c3db42447fda7b2abc
Kanat62 <102471510+Kanat62@users.noreply.github.com>
2023-12-09 23:27:23 +0600

    Update firebase-config.js

--- src/utils/firebase-config.js
+++ src/utils/firebase-config.js
@@ -5,7 +5,7 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDgzy6s-evCP1mjc05uHALX763qx8NoMUs",
-  authDomain: "https://chat-app-lac-ten.vercel.app/",
+  authDomain: "https://chat-app-lac-ten.vercel.app",
   projectId: "chat-app-b6fe6",
   storageBucket: "chat-app-b6fe6.appspot.com",
   messagingSenderId: "528225517704",
